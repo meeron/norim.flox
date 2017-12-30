2c0967ab41c52f511d46c115b4f9967cb9975b4e
Miron Jakubowski <miron.jakubowski@fabres.pl>
2017-12-30 17:04:36 +0100

    Init web project

---
+++ src/norim.flox.web/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace norim.flox.web.Controllers
+{
+    [Route("[controller]")]
+    public class StatusController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public object Get()
+        {
+            return new { version = "1.0.0", status = "OK" };
+        }
+    }
+}
